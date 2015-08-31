024ec87dd8dbd610060ad82d85448ab232c726f6
xlgwr <xlgwr@163.com>
2015-08-31 18:58:01 +0800

    initfirst

---
+++ AutoGetXML/Service1.cs
@@ -0,0 +1,69 @@
+﻿using AutoGetXML.Basic;
+using log4net;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace AutoGetXML
+{
+    public partial class Service1 : ServiceBase
+    {
+        private readonly ILog logger;
+        public static IScheduler scheduler;
+        private readonly WinLogWirter winlogger;
+        public Service1()
+        {
+            InitializeComponent();
+            winlogger = new WinLogWirter();
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            logger = LogManager.GetLogger(GetType());
+            scheduler = StdSchedulerFactory.GetDefaultScheduler();
+        }
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+
+                Exception ex = e.ExceptionObject as Exception;
+                winlogger.LogEvent("来自“AutoGetXML.Service”的全局异常。" + ex.Message + "详细信息如下："
+                                    + Environment.NewLine + "［InnerException］" + ex.InnerException
+                                    + Environment.NewLine + "［Source］" + ex.Source
+                                    + Environment.NewLine + "［TargetSite］" + ex.TargetSite
+                                    + Environment.NewLine + "［StackTrace］" + ex.StackTrace);
+            }
+            catch { }
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            scheduler.Start();
+            logger.Info("Quartz服务成功启动");
+        }
+
+        protected override void OnStop()
+        {
+            try
+            {
+                scheduler.Shutdown();
+                logger.Info("Quartz服务成功终止");
+            }
+            finally { }
+        }
+        protected override void OnPause()
+        {
+            scheduler.PauseAll();
+        }
+
+        protected override void OnContinue()
+        {
+            scheduler.ResumeAll();
+        }
+    }
+}
---
+++ AutoGetXML/test.cs
@@ -0,0 +1,73 @@
+﻿using AutoGetXML.Basic;
+using log4net;
+using Quartz;
+using Quartz.Impl;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+namespace AutoGetXML
+{
+    public class Test
+    {
+        private readonly ILog logger;
+        public static IScheduler scheduler;
+        private readonly WinLogWirter winlogger;
+        public Test()
+        {
+            winlogger = new WinLogWirter();
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+            logger = LogManager.GetLogger(GetType());
+            scheduler = StdSchedulerFactory.GetDefaultScheduler();
+        }
+        public void AllMsg(string message)
+        {
+            logger.Debug(message);
+            logger.Info(message);
+
+        }
+        void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+
+                Exception ex = e.ExceptionObject as Exception;
+                winlogger.LogEvent("来自“AutoGetXML.Service”的全局异常。" + ex.Message + "详细信息如下："
+                                    + Environment.NewLine + "［InnerException］" + ex.InnerException
+                                    + Environment.NewLine + "［Source］" + ex.Source
+                                    + Environment.NewLine + "［TargetSite］" + ex.TargetSite
+                                    + Environment.NewLine + "［StackTrace］" + ex.StackTrace);
+            }
+            catch { }
+        }
+
+        public void OnStart()
+        {
+            scheduler.Start();
+            AllMsg("Quartz服务成功启动.");
+        }
+
+        public void OnStop()
+        {
+            try
+            {
+                scheduler.Shutdown();
+                logger.Info("Quartz服务成功终止");
+            }
+            finally { }
+        }
+        public void OnPause()
+        {
+            scheduler.PauseAll();
+        }
+
+        public void OnContinue()
+        {
+            scheduler.ResumeAll();
+        }
+    }
+}
